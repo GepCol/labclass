1f831d362846c853ff7653ef8100721c19de8c21
GepCol <100402501+GepCol@users.noreply.github.com>
2022-03-09 13:49:41 +0300

    Книга и треугольник.
    Или треугольная книга...?

---
+++ lab_class/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_class {
+    class Book {
+        private string name, author, category;
+        private int pages, year;
+
+        private static int price;
+
+        public Book(string name, string author) {
+            setBook(name, author, "Другое", 0, 0);
+        }
+
+        public Book(string name, string author, string category, int pages, int year) {
+            setBook(name, author, category, pages, year);
+        }
+
+        public void show() {
+            Console.WriteLine($"[{name}] by {author}, {pages} стр.");
+            Console.WriteLine($"Год печати: {year};");
+            Console.WriteLine($"Категория: {category};");
+            Console.WriteLine($"Все книги можно арендовывать по {price}р/день, либо по {price*7}р/неделю.");
+        }
+
+        public void setBook(string name, string author, string category, int pages, int year) {
+            this.name = name;
+            this.author = author;
+            this.category = category;
+            this.pages = pages;
+            this.year = year;
+        }
+
+        static Book() {
+            price = 10;
+        }
+    }
+}
---
+++ lab_class/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab_class {
+    class Program {
+        static void Main(string[] args) {
+            Book book = new Book("Война и Мир", "Л.Н.Толстой", "Художественные", 99999, 2014);
+            book.show();
+            Console.WriteLine("Но кому нужны эти книги? Мы ж технари, давайте треугольники считать! Введите 3 стороны треугольника одной строкой.");
+            string[] sts = Console.ReadLine().Split();
+            Triangle t = new Triangle(double.Parse(sts[0]), double.Parse(sts[1]), double.Parse(sts[2]));
+            Console.WriteLine($"Так, сей треугольник вообще может существовать? Похоже, что {(t.possible()?"да":"нет")}.");
+            Console.WriteLine($"Периметр его будет равен {t.perim()}, а площадь - {t.square()}.");
+            Console.WriteLine($"Вот на что способен наш {t}!");
+
+
+        }
+    }
+}
